cd17e4abb62460ee97eae6a57c9f77d9b25afe9a
Rajgupta7488 <120235145+Rajgupta7488@users.noreply.github.com>
2025-06-21 16:03:15 +0530

    Added UserMapper for converting User entities to DTOs and vice versa

---
+++ src/main/java/ecommerce/util/UserMapper.java
@@ -0,0 +1,34 @@
+package ecommerce.util;
+
+import ecommerce.dto.UserRegistrationRequest;
+import ecommerce.dto.UserResponse;
+import ecommerce.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    public User toEntity(UserRegistrationRequest request) {
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setEmail(request.getEmail());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setPhoneNumber(request.getPhoneNumber());
+        user.setAddress(request.getAddress());
+        return user;
+    }
+
+    public UserResponse toResponse(User user) {
+        UserResponse response = new UserResponse();
+        response.setId(user.getId());
+        response.setUsername(user.getUsername());
+        response.setEmail(user.getEmail());
+        response.setFirstName(user.getFirstName());
+        response.setLastName(user.getLastName());
+        response.setPhoneNumber(user.getPhoneNumber());
+        response.setAddress(user.getAddress());
+        response.setRole(user.getRole());
+        return response;
+    }
+}
