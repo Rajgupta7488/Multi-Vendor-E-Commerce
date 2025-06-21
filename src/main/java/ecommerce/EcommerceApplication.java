fe3ab0743f49537d950ad083ad53818ac5649815
Rajgupta7488 <120235145+Rajgupta7488@users.noreply.github.com>
2025-06-21 15:49:18 +0530

    Enabled JPA Auditing in EcommerceApplication

--- src/main/java/ecommerce/EcommerceApplication.java
+++ src/main/java/ecommerce/EcommerceApplication.java
@@ -2,8 +2,12 @@ package ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@SpringBootApplication
+@EnableJpaAuditing
 public class EcommerceApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(EcommerceApplication.class, args);
     }
