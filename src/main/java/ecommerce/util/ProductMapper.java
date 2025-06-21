538150921fface7267540cce65a8e5a6d217e6b7
Rajgupta7488 <120235145+Rajgupta7488@users.noreply.github.com>
2025-06-21 16:02:48 +0530

    Added ProductMapper for converting Product entities to ProductResponse DTOs

---
+++ src/main/java/ecommerce/util/ProductMapper.java
@@ -0,0 +1,27 @@
+package ecommerce.util;
+
+import ecommerce.dto.ProductResponse;
+import ecommerce.entity.Product;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductMapper {
+
+    public ProductResponse toResponse(Product product) {
+        ProductResponse response = new ProductResponse();
+        response.setId(product.getId());
+        response.setName(product.getName());
+        response.setDescription(product.getDescription());
+        response.setPrice(product.getPrice());
+        response.setStockQuantity(product.getStockQuantity());
+        response.setImageUrl(product.getImageUrl());
+        response.setActive(product.isActive());
+        
+        if (product.getCategory() != null) {
+            response.setCategoryId(product.getCategory().getId());
+            response.setCategoryName(product.getCategory().getName());
+        }
+        
+        return response;
+    }
+}
