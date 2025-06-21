14e3a3a23ae6230488d39f885a6f12cbf5725de4
Rajgupta7488 <120235145+Rajgupta7488@users.noreply.github.com>
2025-06-21 12:13:30 +0530

    Added Cart entity for user cart management
    - Introduced `Cart` entity with fields: id, user, cartItems, totalAmount, createdAt, and updatedAt.
    - Established OneToOne relationship with `User` and OneToMany relationship with `CartItem`.
    - Incorporated Spring Data JPA auditing with `@CreatedDate` and `@LastModifiedDate` annotations.
    - Ensures structured and maintainable design for managing user carts.

---
+++ src/main/java/ecommerce/entity/Cart.java
@@ -0,0 +1,57 @@
+package ecommerce.
+        entity;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "carts")
+@EntityListeners(AuditingEntityListener.class)
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> cartItems;
+
+    private BigDecimal totalAmount = BigDecimal.ZERO;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+
+    public Cart() {}
+
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+    
+    public List<CartItem> getCartItems() { return cartItems; }
+    public void setCartItems(List<CartItem> cartItems) { this.cartItems = cartItems; }
+    
+    public BigDecimal getTotalAmount() { return totalAmount; }
+    public void setTotalAmount(BigDecimal totalAmount) { this.totalAmount = totalAmount; }
+    
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+    
+    public LocalDateTime getUpdatedAt() { return updatedAt; }
+    public void setUpdatedAt(LocalDateTime updatedAt) { this.updatedAt = updatedAt; }
+}
